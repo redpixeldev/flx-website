0b9b5dd043a801300827e24fdd52fc4f6ce2459e
Daniela Guillen <daniela@redpixel.io>
2022-12-18 18:23:31 -0400

    Project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,65 +1,34 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-	future: {
-		hoverOnlywhenSupported: true,
-	},
-	content: ["./src/**/*.{html,njk,md}"],
-	theme: {
-		fontFamily: {
-			body: "",
+  content: ["./src/**/*.{html,njk,md}"],
+  theme: {
+    fontFamily: {
+			notoSerif: ['Noto Serif', 'serif'],
+			sourceSans: ['Source Sans Pro', 'sans-serif'],
+			roboto: ['Roboto', 'sans-serif']
 		},
-
-		container: {
-			center: true,
-			padding: "1rem",
+		screens: {
+			xs: "530px",
+			sm: "640px",
+			md: "768px",
+			desk: "890px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
 		},
 
 		extend: {
-			zIndex: {
-				"-1": "-1",
-			},
-			spacing: {
-				13: "3.25rem",
-				15: "3.75rem",
-				17: "4.25rem",
-				18: "4.5rem",
-				19: "4.75rem",
-				76: "19rem",
-				84: "21rem",
-				88: "22rem",
-				92: "23rem",
-				100: "25rem",
-				104: "26rem",
-				108: "27rem",
-				112: "28rem",
-				116: "29rem",
-				120: "30rem",
-				124: "31rem",
-				128: "32rem",
-				132: "33rem",
-				136: "34rem",
-				140: "35rem",
-				144: "36rem",
-				148: "37rem",
-				152: "38rem",
-				156: "39rem",
-				160: "40rem",
-				164: "41rem",
-				168: "42rem",
-				172: "43rem",
-				176: "44rem",
-				180: "45rem",
-				184: "46rem",
-				188: "47rem",
-				190: "48rem",
-				194: "49rem",
-				200: "50rem",
-			},
-		},
-	},
-	plugins: [
-		require("@tailwindcss/typography"),
-		require("@tailwindcss/aspect-ratio"),
-		require("@tailwindcss/forms"),
-	],
+			colors: {
+				"camBlue": "#17acff",
+				"camDark": "#172030",
+				"camBlack": "#21242b",
+				"camGray": "#6d727e",
+				"camGrayLight": "#fafcff",
+			}
+		}
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
 };
